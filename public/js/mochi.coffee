778b6b9deff37e709067058c276ddb4beae32a23
bdickason <dickason@gmail.com>
2011-09-14 00:00:07 -0400

    Collections now render on load

--- public/js/mochi.coffee
+++ public/js/mochi.coffee
@@ -7,25 +7,37 @@
 $ ->
   
   window.Daily = Backbone.Model.extend {}
+  
+  class window.Reports extends Backbone.Collection
+    model: Daily
+    url: '/api/reports/salesTax/06-01-2011/08-31-2011'
     
-  window.ReportView = Backbone.View.extend
+    
+  class window.ReportView extends Backbone.View
     tagName: 'div'
     className: 'userWrapper'
       
     initialize: ->
-      # @el = '.userWrapper'
+      @el = '.userWrapper'
       _.bindAll this, 'render'
-      @model.bind 'change', @render
+      @collection.bind 'reset', @render
+      @model.view = this
+      
       # Compile Handlebars template at init (Not sure if we have to do this each time or not)
       source = $('#salesTax-template').html()
       @template = Handlebars.compile source
       
+      # Get the latest collections
+      @collection.fetch()
+      
     render: ->
       # Render Handlebars template
+      console.log 'rendering!'
       renderedContent = @template @model.toJSON() #
       $(@el).html renderedContent
       return this
-      
-  window.report = new Daily {dates: {start: 'blah', end: 'blah2'}}
   
-  window.reportView = new ReportView {model: report}
+  window.report = new Daily {dates: {start: 'blah', end: 'blah2'}}
+  window.reports = new Reports
+
+  window.reportView = new ReportView {model: report, collection: reports }
--- public/js/mochi.js
+++ public/js/mochi.js
@@ -1,31 +1,59 @@
 (function() {
-  /* Main client-side application code */  $(function() {
+  /* Main client-side application code */  var __hasProp = Object.prototype.hasOwnProperty, __extends = function(child, parent) {
+    for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; }
+    function ctor() { this.constructor = child; }
+    ctor.prototype = parent.prototype;
+    child.prototype = new ctor;
+    child.__super__ = parent.prototype;
+    return child;
+  };
+  $(function() {
     window.Daily = Backbone.Model.extend({});
-    window.ReportView = Backbone.View.extend({
-      tagName: 'div',
-      className: 'userWrapper',
-      initialize: function() {
+    window.Reports = (function() {
+      __extends(Reports, Backbone.Collection);
+      function Reports() {
+        Reports.__super__.constructor.apply(this, arguments);
+      }
+      Reports.prototype.model = Daily;
+      Reports.prototype.url = '/api/reports/salesTax/06-01-2011/08-31-2011';
+      return Reports;
+    })();
+    window.ReportView = (function() {
+      __extends(ReportView, Backbone.View);
+      function ReportView() {
+        ReportView.__super__.constructor.apply(this, arguments);
+      }
+      ReportView.prototype.tagName = 'div';
+      ReportView.prototype.className = 'userWrapper';
+      ReportView.prototype.initialize = function() {
         var source;
+        this.el = '.userWrapper';
         _.bindAll(this, 'render');
-        this.model.bind('change', this.render);
+        this.collection.bind('reset', this.render);
+        this.model.view = this;
         source = $('#salesTax-template').html();
-        return this.template = Handlebars.compile(source);
-      },
-      render: function() {
+        this.template = Handlebars.compile(source);
+        return this.collection.fetch();
+      };
+      ReportView.prototype.render = function() {
         var renderedContent;
+        console.log('rendering!');
         renderedContent = this.template(this.model.toJSON());
         $(this.el).html(renderedContent);
         return this;
-      }
-    });
+      };
+      return ReportView;
+    })();
     window.report = new Daily({
       dates: {
         start: 'blah',
         end: 'blah2'
       }
     });
+    window.reports = new Reports;
     return window.reportView = new ReportView({
-      model: report
+      model: report,
+      collection: reports
     });
   });
 }).call(this);
